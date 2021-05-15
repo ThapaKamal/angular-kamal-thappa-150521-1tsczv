14e369ce1706489d7f55681702281aa095f0525b
ThapaKamal <thapa.kamal@outlook.com>
2021-05-15 19:23:21 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,40 +3,63 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
-  candidates=[{
-    "id": 1,
-    "first_name": "Jeanette",
-    "last_name": "Penddreth",
-    "email": "jpenddreth0@census.gov",
-    "gender": "Female"
-  }, {
-    "id": 2,
-    "first_name": "Giavani",
-    "last_name": "Frediani",
-    "email": "gfrediani1@senate.gov",
-    "gender": "Male"
-  }, {
-    "id": 3,
-    "first_name": "Noell",
-    "last_name": "Bea",
-    "email": "nbea2@imageshack.us",
-    "gender": "Female"
-  }, {
-    "id": 4,
-    "first_name": "Willard",
-    "last_name": "Valek",
-    "email": "wvalek3@vk.com",
-    "gender": "Male"
-  }];
+  records: Array<any>;
+  isDesc: boolean = false;
+  column: string = 'id';
+  categoris: ['Jeanette', 'Giavani'];
 
+  candidates = [
+    {
+      id: 1,
+      first_name: 'Jeanette',
+      last_name: 'Penddreth',
+      email: 'jpenddreth0@census.gov',
+      gender: 'Female'
+    },
+    {
+      id: 2,
+      first_name: 'Giavani',
+      last_name: 'Frediani',
+      email: 'gfrediani1@senate.gov',
+      gender: 'Male'
+    },
+    {
+      id: 3,
+      first_name: 'Noell',
+      last_name: 'Bea',
+      email: 'nbea2@imageshack.us',
+      gender: 'Female'
+    },
+    {
+      id: 4,
+      first_name: 'Willard',
+      last_name: 'Valek',
+      email: 'wvalek3@vk.com',
+      gender: 'Male'
+    }
+  ];
 
-femaleOnly(event){
-  
-}
+  femaleOnly(event) {}
+  maleOnly(event) {}
 
+  sort(property) {
+    this.isDesc = !this.isDesc; //change the direction
+    this.column = property;
+    let direction = this.isDesc ? 1 : -1;
+
+    this.records.sort(function(a, b) {
+      if (a[property] < b[property]) {
+        return -1 * direction;
+      } else if (a[property] > b[property]) {
+        return 1 * direction;
+      } else {
+        return 0;
+      }
+    });
+  }
 }
